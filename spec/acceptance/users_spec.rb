0fc007b0883cf1187131744cb9dbae3a5fdf24cf
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2010-12-07 21:28:31 +0100

    Rewrite the project fetching

--- spec/acceptance/users_spec.rb
+++ spec/acceptance/users_spec.rb
@@ -11,14 +11,36 @@ feature 'Users', %q{
     Project.create!(:name => 'project1', :user => 'alice')
     Project.create!(:name => 'project2', :user => 'alice')
     Project.create!(:name => 'project3', :user => 'alice')
+
+    FakeWeb.register_uri(:post, 'https://github.com/login/oauth/access_token', :body => 'access_token=github')
+    FakeWeb.register_uri(:get, 'https://github.com/api/v2/json/user/show?access_token=github', :body => '{"user": {"login": "alice"}}')
   end
 
   scenario 'log in via Github' do
-    FakeWeb.register_uri(:post, 'https://github.com/login/oauth/access_token', :body => 'access_token=github')
-    FakeWeb.register_uri(:get, 'https://github.com/api/v2/json/user/show?access_token=github', :body => '{"user": {"login": "alice"}}')
-
     visit '/auth/github/callback'
     page.should have_content 'Hi alice, here\'s a list of every Github project you started.'
+  end
+
+  context 'getting the projects from github' do
+    background do
+      HTTParty.should_receive(:get).and_return(
+        'repositories' => [
+          {'name' => 'fetched_project', 'owner' => 'alice'},
+          {'name' => 'forked_project', 'owner' => 'alice', 'fork' => true}
+        ]
+      )
+    end
+
+    scenario 'show the projects in the form' do
+      visit '/auth/github/callback'
+      page.should have_content 'fetched_project'
+    end
+
+    scenario 'do not show forked projects in the form' do
+      visit '/auth/github/callback'
+      page.should have_no_content 'forked_project'
+    end
+
   end
 
   scenario 'Fill in the edit user form' do
