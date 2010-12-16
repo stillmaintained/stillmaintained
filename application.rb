ded31e793cc2601a5abd3a17095fa5b01837b3e4
Ivana Setiawan <ivana@80beans.com>
2010-12-17 00:11:58 +0100

    homepage styling. almost done :)

--- application.rb
+++ application.rb
@@ -9,6 +9,8 @@ require File.join(File.dirname(__FILE__), 'lib', 'project')
 
 class Application < Sinatra::Base
 
+  set :root, File.dirname(__FILE__)
+
   use HoptoadNotifier::Rack
 
   config = YAML::load_file(File.join(File.dirname(__FILE__), 'config/settings.yml'))
